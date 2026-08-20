bed9ab5d2244afa79e381836c7d6842baa541b16
LJK <jolri24@gmail.com>
2026-08-20 23:21:24 +0900

    설정: 중복 JDBC 스타터 제거

--- adapter-out-persistence/build.gradle.kts
+++ adapter-out-persistence/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:4.1.0"))
     implementation(project(":application"))
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
 
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
